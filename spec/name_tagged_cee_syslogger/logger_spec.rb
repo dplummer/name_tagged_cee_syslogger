5d42eebd8388ebd48801c733e7fe8af856cbde57
Donald Plummer <dplummer@avvo.com>
2015-11-03 09:18:06 -0800

    Tests logging a hash of data

--- spec/name_tagged_cee_syslogger/logger_spec.rb
+++ spec/name_tagged_cee_syslogger/logger_spec.rb
@@ -3,13 +3,23 @@ require "spec_helper"
 describe NameTaggedCeeSyslogger::Logger do
   let(:syslog) { double("syslog", :mask= => true) }
 
-  it "logs simple messages" do
+  before do
     expect(Syslog).to receive(:open).
       with($0, Syslog::LOG_PID | Syslog::LOG_CONS, nil).
       and_yield(syslog)
+  end
+
+  it "logs simple messages" do
     expect(syslog).to receive(:log).
       with(Syslog::LOG_WARNING, '@cee: {"severity":"WARN","msg":"Some message"}')
 
     subject.warn "Some message"
+  end
+
+  it "logs message hashes as data" do
+    expect(syslog).to receive(:log).
+      with(Syslog::LOG_WARNING, '@cee: {"severity":"WARN","foo":"bar","baz":123}')
+
+    subject.warn(foo: "bar", baz: 123)
   end
 end
